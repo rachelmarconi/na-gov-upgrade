88579ecda9433e371232707efbec09873b5f7219
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-05-03 23:17:38 -0400

    finale

--- twitter-bot.py
+++ twitter-bot.py
@@ -2,17 +2,9 @@ import tweepy
 import os
 
 c_key = os.environ['TWEEPY_API_KEY']
-print("c_key: ",c_key)
-
 c_secret = os.environ['TWEEPY_API_SECRET']
-print("c_secret: ",c_secret)
-
 token_key = os.environ['TWEEPY_ACCESS_TOKEN']
-print("token_key: ",token_key)
-
 token_secret = os.environ['TWEEPY_ACCESS_TOKEN_SECRET']
-print("token_secret: ",token_secret)
-
 
 client = tweepy.Client(consumer_key =  c_key,
                         consumer_secret = c_secret,
