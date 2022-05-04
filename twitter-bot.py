a0b30eacfee1c1919c7c51ee7c3de5d1f1469a84
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-05-03 23:09:39 -0400

    Update twitter-bot.py

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,22 +1,12 @@
 import tweepy
 import os
 
-#Add your credentials here
-twitter_keys = {
-        'consumer_key':         os.environ['TWEEPY_API_KEY'],
-        'consumer_secret':      os.environ['TWEEPY_API_SECRET'],
-        'access_token_key':     os.environ['TWEEPY_ACCESS_TOKEN'],
-        'access_token_secret':  os.environ['TWEEPY_ACCESS_TOKEN_SECRET']
-        
+client = tweepy.Client(consumer_key =  os.environ['TWEEPY_API_KEY'],
+                        consumer_secret = os.environ['TWEEPY_API_SECRET'],
+                        access_token = os.environ['TWEEPY_ACCESS_TOKEN'],
+                        access_token_secret = os.environ['TWEEPY_ACCESS_TOKEN_SECRET'])
 
-    }
-
-client = tweepy.Client(consumer_key = twitter_keys['consumer_key'],
-                        consumer_secret = twitter_keys['consumer_secret'],
-                        access_token = twitter_keys['access_token_key'],
-                        access_token_secret = twitter_keys['access_token_secret'])
-
-text = "Hello world! This is another automated tweet!"
+text = "Hello world! This is yet another automated tweet!"
 
 print(text)
 
