405d86bcfd665a055c436bc59a49766fe4ce8ecc
Rachel Logan <56045588+rachelll4@users.noreply.github.com>
2022-05-03 23:14:48 -0400

    test test

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,10 +1,23 @@
 import tweepy
 import os
 
-client = tweepy.Client(consumer_key =  os.environ['TWEEPY_API_KEY'],
-                        consumer_secret = os.environ['TWEEPY_API_SECRET'],
-                        access_token = os.environ['TWEEPY_ACCESS_TOKEN'],
-                        access_token_secret = os.environ['TWEEPY_ACCESS_TOKEN_SECRET'])
+c_key = os.environ['TWEEPY_API_KEY']
+print("c_key: ",c_key)
+
+c_secret = os.environ['TWEEPY_API_SECRET']
+print("c_secret: ",c_secret)
+
+token_key = os.environ['TWEEPY_ACCESS_TOKEN']
+print("token_key: ",token_key)
+
+token_secret = os.environ['TWEEPY_ACCESS_TOKEN_SECRET']
+print("token_secret: ",token_secret)
+
+
+client = tweepy.Client(consumer_key =  c_key,
+                        consumer_secret = c_secret,
+                        access_token = token_key,
+                        access_token_secret = token_secret)
 
 text = "Hello world! This is yet another automated tweet!"
 
